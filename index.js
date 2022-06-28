6630775e46ce7bc58c726c14ee53280c8a337dec
THOMAS HARDING <hardingt604@gmail.com>
2022-06-28 11:28:38 -0600

    add removeTail

--- index.js
+++ index.js
@@ -17,11 +17,33 @@ class LinkedList {
     }
     current.next = new Node(val);
   }
-  removeTail() {}
+  removeTail() {
+    let current = this.head;
+    let temp;
+    if (current === null) {
+      return null;
+      // edge case for 0
+    }
+    if (current.next === null) {
+      temp = current.val;
+      this.head = null;
+      return temp;
+      // edge case for 1
+    }
+    while (current.next.next) {
+      current = current.next;
+    }
+    temp = current.next;
+    current.next = null;
+    return temp.val;
+  }
 
   getList() {
     const listArr = [];
     let current = this.head;
+    if (current === null) {
+      return [];
+    }
     while (current.next) {
       listArr.push(current.val);
       current = current.next;
@@ -37,5 +59,19 @@ list.add("B");
 console.log(list.getList());
 list.add("C");
 console.log(list.getList());
+list.removeTail();
+console.log(list.getList());
+const otherList = new LinkedList(1);
+otherList.removeTail();
+console.log(otherList.getList());
+// just be null
+otherList.removeTail();
+console.log(otherList.getList());
+// also null but without breaking
+const thirdList = new LinkedList(1);
+thirdList.add(2);
+thirdList.removeTail();
+console.log(thirdList.getList());
+// [ 1 ]
 
 module.exports = { LinkedList };
